e810a015473ed8d8429c0fdbc1ee8d31e7fb0a08
Marisol Rodriguez <marisol.rodriguez.mt@correo.usa.edu.co>
2021-10-12 20:46:22 -0500

    solucion reto 3

---
+++ Reto3_Ciclo3/src/main/java/Reto3_Ciclo3/CategoriaWeb.java
@@ -0,0 +1,41 @@
+
+package Reto3_Ciclo3;
+
+import java.util.List;
+import java.util.Optional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+/**
+ * 
+ * @author solecito
+ */
+@RestController
+@RequestMapping("/api/Category")
+public class CategoriaWeb {
+    
+    @Autowired
+    private serviciosCategoria servicios;
+    @GetMapping("/all")
+    public List <Categoria> getCategoria(){
+        return servicios.getAll();
+    }
+    @GetMapping("/{id}")
+    public Optional<Categoria> getCategoria(@PathVariable("id") int idCategoria) {
+        return servicios.getCategoria(idCategoria);
+    }
+
+    @PostMapping("/save")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Categoria save(@RequestBody Categoria categoria) {
+        return servicios.save(categoria);
+    }
+    
+    
+}
---
+++ Reto3_Ciclo3/src/main/java/Reto3_Ciclo3/interfaceCategoria.java
@@ -0,0 +1,12 @@
+
+package Reto3_Ciclo3;
+
+import org.springframework.data.repository.CrudRepository;
+
+/**
+ * 
+ * @author solecito
+ */
+public interface interfaceCategoria extends CrudRepository<Categoria,Integer> {
+    
+}
